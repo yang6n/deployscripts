f902f201538deb7f1df30951c937470c8e74e990
Yang Jian <yang6n@users.noreply.github.com>
2015-08-14 20:45:53 -0500

    Create sshcopyid.sh

---
+++ rundeck/sshcopyid.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+
+if { $argc != 3 } {
+  puts stderr "usage: ./sshcopyid.sh host pass user"
+  exit 2
+}
+
+set host [lindex $argv 0]
+set user [lindex $argv 2]
+set passwd [lindex $argv 1]
+
+spawn ssh-copy-id $user@$host
+expect "assword:"
+send "$pass\n"
+
+expect eof
