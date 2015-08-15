18a6f897efbd55cec4cee882b5baacac0049b772
Yang Jian <yang6n@users.noreply.github.com>
2015-08-14 21:15:17 -0500

    Update sshcopyid.sh

--- rundeck/sshcopyid.sh
+++ rundeck/sshcopyid.sh
@@ -10,10 +10,16 @@ set user [lindex $argv 1]
 set passwd [lindex $argv 2]
 
 spawn ssh-copy-id $user@$host
-expect "Are you sure you want to continue connecting"
-send "yes\n"
-
-expect "assword:"
-send "$passwd\n"
+expect {
+    "Are you sure you want to continue connecting" {
+        send "yes\n" 
+        exp_continue   
+    }
+    "All keys were skipped because they already exist on the remote system" {
+    }
+    "assword:" {
+        send "$passwd\n"
+    }
+}
 
 expect eof
